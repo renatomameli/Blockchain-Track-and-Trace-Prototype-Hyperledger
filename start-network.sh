1e18948ac5dcde58b5fc5cf335a4d02d555536fa
renato.mameli <renato.mameli@de.rhenus.com>
2023-06-16 12:39:55 +0200

    Add TSL

--- start-network.sh
+++ start-network.sh
@@ -2,6 +2,12 @@
 export FABRIC_CFG_PATH=/Users/renato.mameli/Hyperledger/trackandtrace
 export PATH=~/Hyperledger/fabric-samples/bin:$PATH
 
+docker compose down
+rm -r channel-artifacts
+rm -r crypto-config
+rm -r system-genesis-block
+
+
 # Step 1
 cryptogen generate --config=./crypto-config.yaml
 
@@ -14,18 +20,18 @@ configtxgen -profile Frachtverfolgung -outputCreateChannelTx ./channel-artifacts
 # Step 4
 docker-compose up -d
 
-sleep 10
+sleep 5
 
-# Step 7
+# Step 5
 docker exec -it peer0.consignor.trackandtrace.com peer channel create -o orderer.trackandtrace.com:7050 -c trackandtracechannel -f /etc/hyperledger/fabric/channel-artifacts/channel.tx --tls true --cafile /etc/hyperledger/fabric/orderer/tlsca.trackandtrace.com-cert.pem
 
-# Step 8
+# Step 6
 docker exec -it peer0.consignor.trackandtrace.com peer channel join -b /trackandtracechannel.block
 
-# Step 9
+# Step 7
 docker cp peer0.consignor.trackandtrace.com:/trackandtracechannel.block .
 
-# Step 10
+# Step 8
 docker cp ./trackandtracechannel.block peer0.consignee.trackandtrace.com:/trackandtracechannel.block
 docker cp ./trackandtracechannel.block peer0.precarriageshipper.trackandtrace.com:/trackandtracechannel.block
 docker cp ./trackandtracechannel.block peer0.maincarrier.trackandtrace.com:/trackandtracechannel.block
@@ -38,3 +44,8 @@ docker exec -it peer0.precarriageshipper.trackandtrace.com peer channel join -b 
 docker exec -it peer0.maincarrier.trackandtrace.com peer channel join -b /trackandtracechannel.block
 docker exec -it peer0.oncarriageshipper.trackandtrace.com peer channel join -b /trackandtracechannel.block
 docker exec -it peer0.customs.trackandtrace.com peer channel join -b /trackandtracechannel.block
+
+# Step 12 TODO: Add zip creation of java project
+# gradle build
+# zip -r code.zip ./build/libs/chaincode-1.0-SNAPSHOT.jar
+# peer chaincode install -n mychaincode -v 1.0 -p /path/to/chaincode-directory
