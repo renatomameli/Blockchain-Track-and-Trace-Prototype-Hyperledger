d4e3aeda533d4c4e05a35ebc28459e386b160ac2
renato.mameli <renato.mameli@de.rhenus.com>
2023-07-03 22:33:31 +0200

    comments script

--- start-network.sh
+++ start-network.sh
@@ -1,53 +1,52 @@
-# Set Environment variables
+# Setze Umgebungsvariablen
 export FABRIC_CFG_PATH=/Users/renato.mameli/Hyperledger/trackandtrace
 export PATH=~/Hyperledger/fabric-samples/bin:$PATH
 
-# Step 0: Shut down running containers and delete generated configuration files
+# Schritt 0: Fahre laufende Container runter und lösche generierte Konfigurationsdateien
 docker compose down
 rm -r channel-artifacts
 rm -r crypto-config
 rm -r system-genesis-block
 
-
-# Step 1: Generate crypto-configs
+# Schritt 1: Generiere crypto-configs
 cryptogen generate --config=./crypto-config.yaml
 
-# Step 2: Generate Genesis Block
+# Schritt 2: Generiere Genesis Block
 configtxgen -profile SampleMultiNodeEtcdRaft -channelID system-channel -outputBlock ./system-genesis-block/genesis.block
 
-# Step 3: Create channel artifacts
+# Schritt 3: Erstelle Channel-Artifakte
 configtxgen -profile Frachtverfolgung -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID trackandtracechannel
 
-# Step 4: Start network in socker
+# Schritt 4: Starte Netzwerk mit Docker
 docker-compose up -d
 
-# Wait until docker containers have started
+# Warte bis die Docker-Container hochgefahren sind
 sleep 5
 
-# Step 5: Create a channel out of the peer container
+# Schritt 5: Erstelle einen Channel vom Peer Container aus
 docker exec -it peer0.consignor.trackandtrace.com peer channel create -o orderer.trackandtrace.com:7050 -c trackandtracechannel -f /etc/hyperledger/fabric/channel-artifacts/channel.tx --tls true --cafile /etc/hyperledger/fabric/orderer/tlsca.trackandtrace.com-cert.pem
 
-# Step 6: Join the channel with the first peer
+# Schritt 6: Trete dem Channel mit dem ersten Peer bei
 docker exec -it peer0.consignor.trackandtrace.com peer channel join -b /trackandtracechannel.block
 
-# Step 7: Copy the channel block to the local system
+# Schritt 7: Kopiere den Channel-Block auf das lokale System
 docker cp peer0.consignor.trackandtrace.com:/trackandtracechannel.block .
 
-# Step 8. Copy the block at the local system to all other peers
+# Schritt 8. Kopiere den Block vom lokalen System auf die restlichen Peers.
 docker cp ./trackandtracechannel.block peer0.consignee.trackandtrace.com:/trackandtracechannel.block
 docker cp ./trackandtracechannel.block peer0.precarriageshipper.trackandtrace.com:/trackandtracechannel.block
 docker cp ./trackandtracechannel.block peer0.maincarrier.trackandtrace.com:/trackandtracechannel.block
 docker cp ./trackandtracechannel.block peer0.oncarriageshipper.trackandtrace.com:/trackandtracechannel.block
 docker cp ./trackandtracechannel.block peer0.customs.trackandtrace.com:/trackandtracechannel.block
 
-# Step 11: Join the channel from all other peers
+# Schritt 9: Die restlichen Peers treten dem Channel bei
 docker exec -it peer0.consignee.trackandtrace.com peer channel join -b /trackandtracechannel.block
 docker exec -it peer0.precarriageshipper.trackandtrace.com peer channel join -b /trackandtracechannel.block
 docker exec -it peer0.maincarrier.trackandtrace.com peer channel join -b /trackandtracechannel.block
 docker exec -it peer0.oncarriageshipper.trackandtrace.com peer channel join -b /trackandtracechannel.block
 docker exec -it peer0.customs.trackandtrace.com peer channel join -b /trackandtracechannel.block
 
-# Step 12: Package and install chaincode
-#docker exec -it peer0.consignor.trackandtrace.com peer lifecycle chaincode package mychaincode.tar.gz --path /etc/hyperledger/fabric/chaincode/ --lang java --label mychaincode_1
+# Schritt 11: Verpacke and installiere den Chaincode
+# docker exec -it peer0.consignor.trackandtrace.com peer lifecycle chaincode package mychaincode.tar.gz --path /etc/hyperledger/fabric/chaincode/ --lang java --label mychaincode_1
 
 # docker exec -it peer0.consignor.trackandtrace.com peer lifecycle chaincode install mychaincode.tar.gz
